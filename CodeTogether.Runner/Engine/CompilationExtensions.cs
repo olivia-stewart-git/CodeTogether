29164664ee45b1d5b1cfaba5a5bad153010c69cf
Olivia Stewart <liv.stewart2004@gmail.com>
2024-07-31 12:28:14 +1000

    create compilation engine

---
+++ CodeTogether.Runner/Engine/CompilationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace CodeTogether.Runner.Engine;
+public static class CompilationExtensions
+{
+	public static bool AddAssembly(this ISet<MetadataReference> references, Type type)
+	{
+		try
+		{
+			var systemReference = MetadataReference.CreateFromFile(type.Assembly.Location);
+			references.Add(systemReference);
+		}
+		catch
+		{
+			return false;
+		}
+
+		return true;
+	}
+}
--- CodeTogether.Runner/Engine/ICompilationEngine.cs
+++ CodeTogether.Runner/Engine/ICompilationEngine.cs
@@ -1,6 +1,8 @@
-﻿namespace CodeTogether.Runner.Engine;
+﻿using System.Reflection;
+
+namespace CodeTogether.Runner.Engine;
 
 public interface ICompilationEngine
 {
-
+	Assembly? CreateCompilation(string assemblyName, string sourceCode, params Type[] referenceTypes);
 }
