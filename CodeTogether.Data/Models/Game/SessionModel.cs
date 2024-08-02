1c1158d0230fc5eec84c8fb6b1715e85d6a406e2
Oliver Warrick <Oliver.Warrick@wisetechglobal.com>
2024-08-02 18:07:22 +1000

    add basic game model

---
+++ CodeTogether.Data/Models/Game/SessionModel.cs
@@ -0,0 +1,17 @@
+﻿using CodeTogether.Data.DataAccess;
+using CodeTogether.Data.Models.Game;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeTogether.Data.Models.Questions;
+
+[PrimaryKey(nameof(GM_PK))]
+public class GameModel : IDbModel
+{
+	public Guid GM_PK { get; set; } = Guid.NewGuid();
+
+	[MaxLength(100)]
+	public string? GM_Name { get; set; }
+
+	IEnumerable<UserModel> Users { get; set; } = new List<UserModel>();
+}
---
+++ CodeTogether.Data/Models/Game/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using CodeTogether.Data.DataAccess;
+using CodeTogether.Data.Models.Questions;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeTogether.Data.Models.Game
+{
+	[PrimaryKey(nameof(UR_PK))]
+	internal class UserModel : IDbModel
+	{
+		public Guid UR_PK { get; set; } = Guid.NewGuid();
+
+		[MaxLength(100)]
+		public required string UR_Name { get; set; }
+
+		public required GameModel UR_Game { get; set; }
+	}
+}
