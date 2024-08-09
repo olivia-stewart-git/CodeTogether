2c617f10070e1d67c753bb012f79dfb1a560ffc7
Olivia Stewart <liv.stewart2004@gmail.com>
2024-08-09 17:57:08 +1000

    started question sand test view

---
+++ CodeTogether.Client.Integration/Execution/QuestionDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeTogether.Client.Integration.Execution;
+
+[Serializable]
+public class QuestionDTO
+{
+	public Guid Id { get; set; } = Guid.Empty;
+	public string Name { get; set; } = string.Empty;
+	public string Description { get; set; } = string.Empty;
+
+}
