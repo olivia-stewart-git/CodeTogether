dc4e05e300cb2b508ba85f40c6096bf9a49825ca
Olivia Stewart <liv.stewart2004@gmail.com>
2024-08-17 17:53:57 +1000

    update visuals and run

--- CodeTogether.Client.Integration/Execution/ExecutionResponseDTO.cs
+++ CodeTogether.Client.Integration/Execution/ExecutionResponseDTO.cs
@@ -6,4 +6,7 @@ public class ExecutionResponseDTO
 	public string Output { get; set; } = string.Empty;
 	public string State { get; set; } = string.Empty;
 	public TestCaseDto.RunDTO[] TestResults { get; set; } = [];
+
+	Dictionary<Guid, TestCaseDto.RunDTO>? testResultsDictionary;
+	public Dictionary<Guid, TestCaseDto.RunDTO> TestResultsDictionary => testResultsDictionary??= TestResults.ToDictionary(x => x.Id);
 }
