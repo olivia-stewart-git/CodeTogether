0c1d3297ab0768505ca5b50ed4109b4e0aa0111f
Amber Cahill <amber.cahill@wisetechglobal.com>
2024-08-18 19:03:58 +1000

    Clean up and style stats page

--- CodeTogether/Controllers/StatsController.cs
+++ CodeTogether/Controllers/StatsController.cs
@@ -22,18 +22,15 @@ public class StatsController(ApplicationDbContext dbContext) : Controller
 	}
 
 	[HttpGet("games")]
-	public IActionResult GetGames([FromQuery(Name = "pageNum")] int? pageNum, [FromQuery(Name = "pageSize")] int? pageSize)
+	public IActionResult GetGames(int pageNum, int pageSize)
 	{
 		if (!Guid.TryParse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value, out var userId))
 		{
 			return Forbid();
 		}
 
-		var actualPageNum = pageNum ?? 1;
-		var actualPageSize = pageSize ?? 5;
-
 		var gamesPlayed = dbContext.Games.Include(g => g.GamePlayers).Where(g => g.GamePlayers.Any(gp => gp.GMP_USR_FK == userId) && g.GM_FinishedAtUtc != null);
-		var relevant = gamesPlayed.OrderBy(gp => gp.GM_FinishedAtUtc).Skip((actualPageNum - 1) * actualPageSize).Take(actualPageSize);
+		var relevant = gamesPlayed.OrderBy(gp => gp.GM_FinishedAtUtc).Skip((pageNum - 1) * pageSize).Take(pageSize);
 		return Ok(relevant.Select(g => new GameResultDTO
 		{
 			GameName = g.GM_Name,
