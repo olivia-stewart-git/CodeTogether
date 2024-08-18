76851e785265d91ad1ab6d4200c2577ff0d37215
Amber Cahill <amber.cahill@wisetechglobal.com>
2024-08-18 15:20:46 +1000

    Add stats page and controller

---
+++ CodeTogether/Controllers/StatsController.cs
@@ -0,0 +1,14 @@
+﻿using CodeTogether.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeTogether.Controllers;
+
+[Route("api/stats")]
+public class StatsController(ApplicationDbContext DBContext) : Controller
+{
+	[HttpGet("stats")]
+	public IActionResult GetStats()
+	{
+		return Ok();
+	}
+}
