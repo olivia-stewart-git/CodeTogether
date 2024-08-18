54ee411607d03a60b8cb4154dd9d61e2fdbf4d4d
Amber Cahill <amber.cahill@wisetechglobal.com>
2024-08-18 14:52:10 +1000

    Add editor settings synced with localStorage

--- CodeTogether.Client/Program.cs
+++ CodeTogether.Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Not sure if this is required but its what is in
 // https://github.com/dotnet/blazor-samples/blob/main/8.0/BlazorSignalRApp/BlazorSignalRApp.Client/Program.cs
 
+using Blazored.LocalStorage;
 using CodeTogether.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,6 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 // for some reason ened both this one and the one in the backend?
 builder.Services
 	.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
-	.AddScoped<UserStateService>();
+	.AddScoped<UserStateService>()
+	.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
